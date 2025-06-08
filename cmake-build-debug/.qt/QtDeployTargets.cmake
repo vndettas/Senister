2304b09575acc3c879ec9f0b8664afeb5af4ed44
vndettas <vovabaranetski@gmail.com>
2025-06-08 14:46:18 +0200

    Remove .idea and build directories from repository

--- cmake-build-debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Veditor_FILE C:/Users/vovab/Documents/GitHub/Veditor/cmake-build-debug/Veditor.exe)
-set(__QT_DEPLOY_TARGET_Veditor_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Veditor_RUNTIME_DLLS D:/Qt/6.8.2/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.8.2/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.8.2/mingw_64/bin/Qt6Core.dll)
