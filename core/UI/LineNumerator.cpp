8b5e99d41e2ac5a5633c439cce07400d766e84b7
vndettas <vovabaranetski@gmail.com>
2025-06-18 14:44:32 +0200

    Rebuilded numeration, delegated it to another class, fixed bugs

---
+++ core/UI/LineNumerator.cpp
@@ -0,0 +1,5 @@
+//
+// Created by vovab on 18.06.2025.
+//
+
+#include "LineNumerator.h"
---
+++ core/UI/LineNumerator.h
@@ -0,0 +1,14 @@
+//
+// Created by vovab on 18.06.2025.
+//
+
+#ifndef VEDITOR_LINENUMERATOR_H
+#define VEDITOR_LINENUMERATOR_H
+
+
+class LineNumerator {
+
+};
+
+
+#endif //VEDITOR_LINENUMERATOR_H
