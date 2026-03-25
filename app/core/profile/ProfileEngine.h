564cf8bc08ead74520afa07c0f944dfb2cf373d6
vndettas <vovabaranetski@gmail.com>
2026-03-25 22:43:03 +0100

    feat(): created Profile and ProfileManager classes, profiles directory and json config

---
+++ app/core/profile/ProfileEngine.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <iostream>
+
+
+class Profile {
+public:
+
+  
+
+
+private:
+
+    QString                             profile_name;
+
+    QFont                               profile_font;
+
+    QString                             background_music_path;
+
+    QString                             interactive_sounds_path;
+    
+    //Every profile should have theme
+    
+    //I dont have much friends so i code
+
+};
+
+
+class ProfileEngine {
+public:
+
+
+private:
+
+
+
+};
